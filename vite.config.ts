298ce86bcae4e2ce0f826ebfab3544c42eb00a32
Ford Torion <clifford.torion04@gmail.com>
2025-10-16 12:47:18 +0800

    vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VISE_BASE_PATH || "/Ford-Torion-Website-Portfolio"
 })
